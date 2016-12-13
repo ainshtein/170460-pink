8036d10bc6cab13c8f04be872cee573cfcec37e3
Andrej Amelichkin <ainshtein2004@gmail.com>
2016-12-13 22:23:42 +0200

    remade table, fix header logo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var navMain = document.querySelector(".nav-main");
 var navToggle = document.querySelector(".nav-main__toggle");
+var logo = document.querySelector(".logo");
 
 navMain.classList.remove("nav-main--nojs");
 
@@ -7,8 +8,10 @@ navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("nav-main--closed")) {
     navMain.classList.remove("nav-main--closed");
     navMain.classList.add("nav-main--opened");
+    logo.classList.add("logo--nav-opened");
   } else {
     navMain.classList.add("nav-main--closed");
     navMain.classList.remove("nav-main--opened");
+    logo.classList.remove("logo--nav-opened");
   }
 });
